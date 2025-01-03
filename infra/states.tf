45bad7f583d6f5d1b5cf65a91bd87117e956714a
Tom Le <minh.thong.le@oracle.com>
2025-01-03 14:59:28 +0700

    change states

--- infra/states.tf
+++ infra/states.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "terraform-remote-state"        # Replace with your S3 bucket name
+    bucket         = "changingroom-state"        # Replace with your S3 bucket name
     key            = "infra/terraform.tfstate"      # Path to the state file in the bucket
     region         = "us-west-2"                    # AWS region
-    dynamodb_table = "terraform-state-lock"         # DynamoDB table for locking
+    dynamodb_table = "changingroom-state-lock"         # DynamoDB table for locking
     encrypt        = true                           # Enable state file encryption
   }
 }
